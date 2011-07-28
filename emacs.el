5e764e5fb52a0149c725984c133968b1c6177899
Vinay Patel <mail@vinayvinay.com>
2011-07-28 23:16:51 +0530

    adding js2 haml and sass modes

--- emacs.el
+++ emacs.el
@@ -2,8 +2,14 @@
 (require 'ido)
 (ido-mode t)
      
-;; Rinari
+(add-to-list 'load-path "/usr/share/emacs/site-lisp")
 (add-to-list 'load-path "~/.emacs.d/vendor/rinari")
+(add-to-list 'load-path "~/.emacs.d/vendor/emacs-js2-mode")
+(add-to-list 'load-path "~/.emacs.d/vendor/haml-mode")
+(add-to-list 'load-path "~/.emacs.d/vendor/sass-mode")
+
 (require 'rinari)
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (setq rinari-tags-file-name "TAGS")
